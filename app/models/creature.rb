7bd4de57f39e765c3b46b2b930ba488db1c88696
Morten Salvesen <msalvefirm@gmail.com>
2018-06-19 13:43:01 +0200

    Fixed model Creature for picture_uploader. Changed seed file to include picture. WARNING: UPLOADS THE SAME PICTURE TO CLOUDINARY FOR EACH CREATURE

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -2,9 +2,10 @@ class Creature < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
+  # Carrierwave uploader for Cloudinary
+  mount_uploader :picture, PhotoUploader
+
   validates :name, :address, :picture, :price, presence: true
   #validates :price, numericality: { greater_than: 0 }
 
-  # Uploader for Cloudinary
-  mount_uploader :photo, PhotoUploader
 end
