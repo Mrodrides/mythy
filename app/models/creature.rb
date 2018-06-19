e0fca0a945180c221296088aea378d19056985d3
Morten Salvesen <msalvefirm@gmail.com>
2018-06-19 12:58:05 +0200

    Carrierwave photo uploader installed and added to model Creature.

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -4,4 +4,7 @@ class Creature < ApplicationRecord
 
   validates :name, :address, :picture, :price, presence: true
   #validates :price, numericality: { greater_than: 0 }
+
+  # Uploader for Cloudinary
+  mount_uploader :photo, PhotoUploader
 end
