db19faf1984db6419b40458f45b30ae907b26ef4
Miguel Rodriguez <xan2299@gmail.com>
2018-06-19 16:19:23 +0200

    Geocoding

--- app/models/creature.rb
+++ app/models/creature.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+require 'json'
+
 class Creature < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
@@ -7,4 +10,17 @@ class Creature < ApplicationRecord
 
   validates :name, :address, :picture, :price, presence: true
   #validates :price, numericality: { greater_than: 0 }
+
+  def set_coordinates?
+    url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{self.address}"
+    address_serialized = open(url).read
+    address = JSON.parse(address_serialized)
+    unless address_serialized.include?("error")
+      @address = address["results"][0]["formatted_address"]
+      @lat = address["results"][0]["geometry"]["location"]["lat"].to_f
+      @long = address["results"][0]["geometry"]["location"]["lng"].to_f
+      return true
+    end
+    return false
+  end
 end
