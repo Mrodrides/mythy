7b49f9c24d79551c2cb78db8f574a68c5196fd91
clara schvartzman <c.s.benitez@outlook.com>
2018-06-19 16:16:51 +0200

    Change to route Search

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'creatures#index'
   resources :creatures do
-    collection do                       
-      get 'search', to: "creatures#search", :as => 'search_creatures'
-    end    
+    collection do
+      get 'search', to: "creatures#search", :as => 'search'
+    end
   end
 
   resources :bookings, only: [:index, :create, :update, :destroy]
