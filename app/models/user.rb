d32c164f1d4b8c4eaf6935b14156b331883b6df4
Miguel Rodriguez <xan2299@gmail.com>
2018-06-20 13:55:20 +0200

    Added user details and navbar Photo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  geocoded_by :address, latitude: :lat, longitude: :long
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :creatures
   has_many :bookings
+  mount_uploader :picture, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
